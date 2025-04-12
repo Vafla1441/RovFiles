cad9ad51d6d8fb44d32b9b955b67da186e50b4a3
Vafla1441 <fedorn204@gmail.com>
2025-04-12 14:33:25 +1000

    По Для Nano

---
+++ adv_firmware.ino
@@ -0,0 +1,82 @@
+#define PHSensorPin  A2    //dissolved oxygen sensor analog output pin to arduino mainboard
+#define VREF 5.0    //for arduino uno, the ADC reference is the AVCC, that is 5.0V(TYP)
+#define OFFSET 0.00  //zero drift compensation
+
+#define SCOUNT  30           // sum of sample point
+int analogBuffer[SCOUNT];    //store the analog value in the array, readed from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0,copyIndex = 0;
+
+float averageVoltage,phValue;
+String phstr;
+char z;
+
+void setup()
+{
+    pinMode(PHSensorPin,INPUT);
+    Serial.begin(9600);
+    pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+   static unsigned long analogSampleTimepoint = millis();
+   if(millis()-analogSampleTimepoint > 30U)     //every 30 milliseconds,read the analog value from the ADC
+   {
+     analogSampleTimepoint = millis();
+     analogBuffer[analogBufferIndex] = analogRead(PHSensorPin);    //read the analog value and store into the buffer
+     analogBufferIndex++;
+     if(analogBufferIndex == SCOUNT)
+         analogBufferIndex = 0;
+   }
+   static unsigned long printTimepoint = millis();
+   if(millis()-printTimepoint > 1000U)
+   {
+      printTimepoint = millis();
+      for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
+      {
+        analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
+      }
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the value more stable by the median filtering algorithm
+      phValue = 3.5 * averageVoltage + OFFSET;
+      phstr = String(phValue);
+      Serial.print(phstr);
+      delay(100);
+   }
+   if (Serial.available() > 0) {
+     z = char(Serial.read());
+     if (z == 'A'){
+       digitalWrite(6, HIGH);
+     }
+     if (z == 'B'){
+       digitalWrite(6, LOW);
+     }
+   }
+}
+
+int getMedianNum(int bArray[], int iFilterLen)
+{
+      int bTab[iFilterLen];
+      for (byte i = 0; i<iFilterLen; i++)
+      {
+      bTab[i] = bArray[i];
+      }
+      int i, j, bTemp;
+      for (j = 0; j < iFilterLen - 1; j++)
+      {
+      for (i = 0; i < iFilterLen - j - 1; i++)
+          {
+        if (bTab[i] > bTab[i + 1])
+            {
+        bTemp = bTab[i];
+            bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+         }
+      }
+      }
+      if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+      else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+      return bTemp;
+}
